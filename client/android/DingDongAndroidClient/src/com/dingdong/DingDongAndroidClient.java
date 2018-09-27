1e8e11ab6860821e819a1f1cc6e9d9e64d9f7a16
gordonyellow <gordonyellow@139.com>
2018-09-28 00:28:07 +0800

    调整安卓客户端代码

--- client/android/DingDongAndroidClient/src/com/dingdong/DingDongAndroidClient.java
+++ client/android/DingDongAndroidClient/src/com/dingdong/DingDongAndroidClient.java
@@ -12,11 +12,7 @@ import android.text.TextUtils;
 import android.util.Log;
 import java.util.List;
 import java.util.ArrayList;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
@@ -26,15 +22,20 @@ import com.dingdong.util.ToastUtil;
 
 public class DingDongAndroidClient extends Activity{
 
-    public static final String SECRET_KEY = "yyyw_dingdong_abc123@@"; //密钥
+    public static final String SECRET_KEY = "dingdong_abc@@123"; //密钥
     public static final String SPLIT_FLAG = "____@@@____"; //分隔符
-    public static final String SERVER_HOST = "192.168.1.177"; //主机host
+    public static final String SERVER_HOST = "127.0.0.1"; //主机host
     public static final int SERVER_PORT = 1234; //主机端口
     private static final String TAG = DingDongAndroidClient.class.getName();
+    private static final String FILE_PATH = "/mnt/sdcard/netease/cloudmusic/Music/任月丽 - 天使的翅膀.mp3";
 
     private TextView cmdTextView;
     private EditText msgEditText;
     private Context ctx;
+
+    private Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -47,14 +48,24 @@ public class DingDongAndroidClient extends Activity{
         msgEditText = (EditText) this.findViewById(R.id.msgEditText);
 
         final List<Object[]> cmdInfoList = new ArrayList<Object[]>();
-        cmdInfoList.add(new Object[] { "PlayMusicThread", "播放音乐" });
-        cmdInfoList.add(new Object[] { "StopPlayMusicThread", "停止播放音乐" });
-        cmdInfoList.add(new Object[] { "SaySthThread", "播放文字" });
-        cmdInfoList.add(new Object[] { "StopSayThread", "停止播放文字" });
+        cmdInfoList.add(new Object[] { "PlayMusic", "播放音乐" });
+        cmdInfoList.add(new Object[] { "StopPlayMusic", "停止播放音乐" });
+        cmdInfoList.add(new Object[] { "SaySth", "播放文字" });
+        cmdInfoList.add(new Object[] { "StopSaySth", "停止播放文字" });
+        cmdInfoList.add(new Object[] { "PlayVoice", "播放声音文件" });
+        cmdInfoList.add(new Object[] { "StopPlayVoice", "停止播放声音文件" });
 
         final String[] cmdItems = new String[cmdInfoList.size()];
         for (int i = 0; i < cmdInfoList.size(); i++) {
             cmdItems[i] = (String) cmdInfoList.get(i)[1];
+        }
+
+        try{
+            socket = new Socket(SERVER_HOST, SERVER_PORT);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+        }catch(IOException ex){
+            ex.printStackTrace();
         }
 
         findViewById(R.id.chooseCmdBtn).setOnClickListener(new View.OnClickListener() {
@@ -75,30 +86,60 @@ public class DingDongAndroidClient extends Activity{
             @Override
             public void onClick(View arg0){
                 try{
-                    //建立socket连接并发送数据
+                    //发送数据
                     String opt = (String)cmdTextView.getTag();
                     String optParams = "";
-                    if("SaySthThread".equals(opt)){
+                    if("SaySth".equals(opt)){
                         optParams = msgEditText.getText().toString();
                     }
+                    if("PlayVoice".equals(opt)){
+                        optParams = "size=" + new File(FILE_PATH).length() + "&type=mp3";
+                    }
                     Log.d(TAG, "opt=" + opt + " & optParams=" + optParams);
-                    Socket socket = new Socket();
-                    socket.connect(new InetSocketAddress(SERVER_HOST, SERVER_PORT), 5000);
-                    PrintWriter out = new PrintWriter(socket.getOutputStream());
-                    out.print(genDatas(opt, optParams) + "\r\n");
-                    out.flush();
-                    System.out.println(" client send: opt=" + opt + "&optParams," + optParams);
+                    if (socket.isConnected()) {
+                        if (!socket.isOutputShutdown()) {
+                            out.println(genDatas(opt, optParams));
+                            out.flush();
+                            Log.d(TAG, "client send: opt=" + opt + " & optParams=" + optParams);
+
+                            if("PlayVoice".equals(opt)){
+                                DataInputStream fis = new DataInputStream(
+                                    new BufferedInputStream(new FileInputStream(FILE_PATH)));
+                                DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+                                int bufferSize = 8192;
+                                byte[] buf = new byte[bufferSize];
+                                while(true){
+                                    int read = 0;
+                                    if (fis != null) {
+                                        read = fis.read(buf);
+                                    }
+                                    if (read == -1) {
+                                        break;
+                                    }
+                                    dos.write(buf,0,read);
+                                }
+                                dos.flush();
+                            }
+                        }else{
+                            Log.d(TAG, "socket output shutdowned");
+                        }
+                    }else{
+                        Log.d(TAG, "socket not connected");
+                    }
 
                     //接收数据
+                    /*
                     BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                     String info = null;
                     while((info=reader.readLine()) != null){
-                        System.out.println("Received from server:" + info);
+                        Log.d(TAG, "Received from server:" + info);
+                        ToastUtil.showMessage(ctx, info);
                     }
+                    */
 
                     //关闭连接
-                    out.close();
-                    socket.close();
+                    //out.close();
+                    //socket.close();
                 }catch(Exception e){
                     System.out.println(e);
                     ToastUtil.showMessage(ctx, e.getMessage());
